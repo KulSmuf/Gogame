0a7be5c083f08097585dce72f413bf16fd2b8f35
Adam Śliwka <asliwka99@gmail.com>
2019-12-03 21:31:59 +0100

    zmiany ClientServerCommunicationTest

--- GUI/src/test/java/client/server/ClientServerCommunicationTest.java
+++ GUI/src/test/java/client/server/ClientServerCommunicationTest.java
@@ -24,9 +24,91 @@ public class ClientServerCommunicationTest {
 		player2.sendCommand("9 p");
 		
 		while( !player1.hasServerSendCommand() );
-		System.out.println("command from server " + player1.getServerCommand());
+		System.out.println("command from server to player1 " + player1.getServerCommand());
 		
 		while( !player2.hasServerSendCommand() );
-		System.out.println("command from server " + player2.getServerCommand());
+		System.out.println("command from server to player2 " + player2.getServerCommand());
+		
+		//   Ruch gracza pierwszego
+		String move = "0 0";
+		player1.sendCommand(move); System.out.println("player1 move: " + move);
+		
+		while( !player1.hasServerSendCommand() );
+		while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+		while( !player2.hasServerSendCommand() );
+		while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+		System.out.println();
+		
+		//   Ruch gracza drugiego
+		move = "0 1";
+		player2.sendCommand(move); System.out.println("player2 move: " + move);
+		
+		while( !player1.hasServerSendCommand() );
+		while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+		while( !player2.hasServerSendCommand() );
+		while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+		System.out.println();
+		
+		//   Ruch gracza pierwszego
+		move = "1 1";
+		player1.sendCommand(move); System.out.println("player1 move: " + move);
+		
+		while( !player1.hasServerSendCommand() );
+		while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+		while( !player2.hasServerSendCommand() );
+		while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+		System.out.println();
+		
+		//   Ruch gracza drugiego
+		move = "1 0";
+		player2.sendCommand(move); System.out.println("player2 move: " + move);
+		
+		while( !player1.hasServerSendCommand() );
+		while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+		while( !player2.hasServerSendCommand() );
+		while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+		System.out.println();
+		
+		//   Zły ruch gracza pierwszego
+		move = "0 0";
+		player1.sendCommand(move); System.out.println("player1 move: " + move);
+		
+		while( !player1.hasServerSendCommand() );
+		while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+		while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+		System.out.println();
+		
+	//   Ruch gracza pierwszego
+			move = "0 2";
+			player1.sendCommand(move); System.out.println("player1 move: " + move);
+			
+			while( !player1.hasServerSendCommand() );
+			while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+			while( !player2.hasServerSendCommand() );
+			while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+			System.out.println();
+			
+		//   Ruch gracza drugiego
+			move = "2 0";
+			player2.sendCommand(move); System.out.println("player2 move: " + move);
+			
+			while( !player1.hasServerSendCommand() );
+			while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+			while( !player2.hasServerSendCommand() );
+			while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+			System.out.println();
+			
+		//   Ruch gracza pierwszego
+			move = "0 0";
+			player1.sendCommand(move); System.out.println("player1 move: " + move);
+			
+			while( !player1.hasServerSendCommand() );
+			while(  player1.hasServerSendCommand() ) System.out.println("command from server to player1 " + player1.getServerCommand());
+			while( !player2.hasServerSendCommand() );
+			while(  player2.hasServerSendCommand() ) System.out.println("command from server to player2 " + player2.getServerCommand());
+			System.out.println();
+			
+			player2.sendCommand("exit");
+			player1.sendCommand("exit");
 	}
 }
